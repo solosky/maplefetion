9042e2af8eec517f9162b4aca1acfa58e62a3df5
zhoushuqun <zhoushuqun@e86f676c-d71c-11de-9f34-657d07d5c07a>
2010-09-11 10:30:40 +0000

    修改shell文件编码为UTF-8。

--- shell/FetionRobotDemo.sh
+++ shell/FetionRobotDemo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ..
-java -cp .:./lib/jdom.jar:./lib/log4j-1.2.15.jar:./dist/maplefetion-core-2.0.1.jar:./dist/maplefetion-demo-2.0.1.jar net.solosky.maplefetion.demo.robot.FetionRobot 15982070573 xu1234 �������ֻ�� ����
+java -cp .:./lib/jdom.jar:./lib/log4j-1.2.15.jar:./dist/maplefetion-core-2.0.1.jar:./dist/maplefetion-demo-2.0.1.jar net.solosky.maplefetion.demo.robot.FetionRobot 15982070573 xu1234 机器人手机号 密码
