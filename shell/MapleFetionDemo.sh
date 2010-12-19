cae77f666d33904870b6bdc39d689e2779bf6b11
solosky.xu <solosky.xu@gmail.com>
2010-12-19 12:00:32 +0000

    创建maplefetion-2.0.1的tags

--- shell/MapleFetionDemo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ..
-java -cp .:./lib/jdom.jar:./lib/log4j-1.2.15.jar:./dist/maplefetion-core-2.0.1.jar:./dist/maplefetion-demo-2.0.1.jar net.solosky.maplefetion.demo.MapleFetion
--- shell/SimpleFetionDemo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ..
-java -cp .:./lib/jdom.jar:./lib/log4j-1.2.15.jar:./dist/maplefetion-core-2.0.1.jar:./dist/maplefetion-demo-2.0.1.jar net.solosky.maplefetion.demo.SimpleFetion 手机号或者飞信号 密码 发送好友的手机号码 发送的消息内容
